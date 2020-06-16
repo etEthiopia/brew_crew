b146866e9ccd133e04bdb3d215983ca237880580
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-16 21:20:14 +0300

    Structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Return after Home or Authenticate
+    return Home();
+  }
+}
