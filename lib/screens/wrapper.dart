ef48f2d4d1861586f90973478d3055e8193f0053
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-06-17 15:46:14 +0300

    Provider, listening to auth changes

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,15 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authentication/authenticate.dart';
 import 'package:flutter/material.dart';
-import 'home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     // Return after Home or Authenticate
+
+    print(user);
     return Authenticate();
   }
 }
